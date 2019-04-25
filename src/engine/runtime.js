78c2a96cc1a3ba4dadafd12c8ae455173281b8d4
Michael "Z" Goddard <mzgoddard@gmail.com>
2019-04-25 10:40:08 -0400

    elevate stack block id and frame info into thread
    - Add pointer member to thread. This is the current executing block.
    - Add stackFrame member to thread. This is the current frame
      information like procedure parameters.
    
    This is a step potentially towards stack-less threads. With further
    modifications we could have stack and stackFrames be null if a script
    does not call a procedure.

--- src/engine/runtime.js
+++ src/engine/runtime.js
@@ -1659,7 +1659,7 @@ class Runtime extends EventEmitter {
     isActiveThread (thread) {
         return (
             (
-                thread.stack.length > 0 &&
+                thread.stackFrame !== null &&
                 thread.status !== Thread.STATUS_DONE) &&
             this.threads.indexOf(thread) > -1);
     }
@@ -1837,6 +1837,9 @@ class Runtime extends EventEmitter {
             // Start the thread with this top block.
             newThreads.push(this._pushThread(topBlockId, target));
         }, optTarget);
+
+        // TODO: Move the execute call to sequencer. Maybe in a method call
+        // stepHat or stepOne.
         newThreads.forEach(thread => {
             // Store hat block fields that must match on the thread, so they can be accessed at any time, even after
             // e.g. we've waited on a promise from an asynchronous reporter. When the hat block's inputs are interpreted
@@ -1847,8 +1850,11 @@ class Runtime extends EventEmitter {
             // For compatibility with Scratch 2, edge triggered hats need to be processed before
             // threads are stepped. See ScratchRuntime.as for original implementation
             const topBlock = thread.blockContainer.getBlock(thread.topBlock);
-            if (this.getIsEdgeActivatedHat(topBlock.opcode)) {
+            if (this.getIsEdgeActivatedHat(topBlock.opcode) && thread.status !== Thread.STATUS_DONE) {
                 thread.goToNextBlock();
+                if (thread.stackFrame === null) {
+                    this.sequencer.retireThread(thread);
+                }
             }
         });
         return newThreads;
